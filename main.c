2df954673cfd4894de58412124058e83e6fa5281
Jeimison Moreno <jeimison.ti@gmail.com>
2019-11-04 03:27:00 -0300

    Funcoes de delay e piscar led prontos

--- main.c
+++ main.c
@@ -6,12 +6,24 @@
 
 #define GPIO(X,Y) ((X*32)+Y)
 
+#define delayMicrosseconds(US, CLK_TIME) \
+        CLK_TIME = clock(); \
+		while( clock() - CLK_TIME <= US) { }
+
+#define delay(MS, CLK_TIME) \
+        delayMicrosseconds(MS*1000, CLK_TIME)
+
 int main(){
+    clock_t timerClk;
     pinMode(GPIO(1,21), OUTPUT);
     pinMode(GPIO(1,22), OUTPUT);
     pinMode(GPIO(1,23), OUTPUT);
     pinMode(GPIO(1,24), OUTPUT);
 
+    while(1){
+        digitalWrite(GPIO(1,21), !digitalRead(GPIO(1,21)) );
+        delay(1000, timerClk);
+    }
     
     return 0;
 }
